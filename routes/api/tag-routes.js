a59741d3e1d96fcacc35d02578ab76c968ce1329
WickerPrison <cobylax17@gmail.com>
2024-03-29 07:29:48 -0600

    added tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,6 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
   const categories = await Tag.findAll({include: [{model: Product}]}).catch((err) => {
     res.json(err);
   });
@@ -14,23 +13,54 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
   const categories = await Tag.findByPk(req.params.id, {include: [{model: Product}]}).catch((err) => {
     res.json(err);
   });
   res.json(categories);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  const newTag = await Tag.create(req.body).catch((err) => {
+    res.status(400).json(err);
+  });
+  res.status(200).json(newTag);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  const tag = await Tag.update(req.body, {
+    where:{
+      id: req.params.id
+    }
+  }).catch((err) =>{
+    res.json(err);
+  });
+
+  if(!tag[0]){
+    res.status(404).json({message: "Tag update failed"});
+    return;
+  }
+
+  res.status(200).json({message: "Tag updated"});
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  const tag = await Tag.destroy({
+    where:{
+      id: req.params.id
+    }
+  }).catch((err) => {
+    res.json(err);
+  });
+
+  if(!tag){
+    res.status(404).json({message: "Tag deletion failed"});
+    return;
+  }
+
+  res.status(200).json({message: "Tag deleted"});
 });
 
 module.exports = router;
